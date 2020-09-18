045f33152ee74d4102dfdbc681a7b43f527942c3
Element04519 <element04519@gmail.com>
2020-09-18 21:05:11 +0200

    Switched to Google voice Recognition

--- main.py
+++ main.py
@@ -2,22 +2,19 @@ from time import sleep
 import speech_recognition as sr
 import mapper
 
-
 def callback(_, audio):
 	try:
-	    result = r.recognize_sphinx(audio, keyword_entries=KEYWORDS)
-	    for word in result.split(' '):
-	    	mapper.word_found(word)
-
+		result = r.recognize_google(audio)
+		print(result)
+		
+		for word in result.split(' '):
+			mapper.word_found(word)
+	
 	except sr.UnknownValueError:
 	    print("Sphinx could not understand audio")
 	except sr.RequestError as e:
-	    print("Sphinx error; {0}".format(e))
+	    print(" error; {0}".format(e))
 
-
-
-
-KEYWORDS = mapper.get_keywords()
 r = sr.Recognizer()
 mic = sr.Microphone()
 
--- mapper.py
+++ mapper.py
@@ -1,31 +1,22 @@
 import output
 
-keywords = {
-	'cowk':'coke',
-	'coke':'coke',
-	'mate':'mate',
-	'marta':'mate',
-	'hackathon':'hackathon',
-	'hackasin':'hackathon',
-	'hekesin': 'hackathon'
-}
+
 
 keyword_to_fun = {
-	'coke' : 'led_on',
-	'mate' : 'dont_puke',
-	'hackathon' : 'puke'
+	'coke' : output.led_on,
+	'mate' : output.dont_puke,
+	'hackathon' : output.puke
 }
 
 def get_keywords():
-	def gen_kw_entries(keys, sensitivity):
-		return [(k,sensitivity) for k in keys]
+	return keyword_to_fun.keys()
 
-	return gen_kw_entries(keywords.values(),0.2)
 
 def word_found(word):
-	if word in keywords:
-		keyword_found(keywords[word])
+	if word in keyword_to_fun.keys():
+		keyword_found(word)
 
 
 def keyword_found(kw):
-	eval("output."+keyword_to_fun[kw]+"()")
+	print(kw)
+	keyword_to_fun[kw]()
