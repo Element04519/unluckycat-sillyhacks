380413ee2252e45db198dbd219c4718677c2f38c
Wutchilli <>
2020-09-19 22:49:45 +0200

    pwm fixed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from time import sleep
 import speech_recognition as sr
 import mapper
+import output
 
 #import sounddevice as sd
 import numpy as np
@@ -9,57 +10,58 @@ import numpy as np
 
 LIGHT_GPIO = 5
 def count_keyword_usage(recognized, keys):
-	counting = dict()
+    counting = dict()
 
-	for entry in recognized:
-		text = entry['transcript']
-		current_counting = dict()
+    for entry in recognized:
+        text = entry['transcript']
+        current_counting = dict()
 
-		for word in text.lower().split(' '):
-			if word.lower() in mapper.keyword_dict:
-				if not word in current_counting:
-					current_counting[word] = 0
+        for word in text.lower().split(' '):
+            if word.lower() in mapper.keyword_dict:
+                if not word in current_counting:
+                    current_counting[word] = 0
 
-				current_counting[word] += 1
+                current_counting[word] += 1
 
-		for word in current_counting.keys():
-			if not word in counting:
-				counting[word] = 0
+        for word in current_counting.keys():
+            if not word in counting:
+                counting[word] = 0
 
-			counting[word] = max(current_counting[word],counting[word])
+            counting[word] = max(current_counting[word],counting[word])
 
-	return counting
+    return counting
 
 
 
 
 def word_listening_callback(_, audio):
-	print("calback triggered")
-	try:
-		result = r.recognize_google(audio, show_all=True)
-		print(result)
+    print("calback triggered")
+    try:
+        result = r.recognize_google(audio, show_all=True)
+        print(result)
 
-		if result == []:
-			return
+        if result == []:
+            return
 
-		result_by_words = count_keyword_usage(result['alternative'], mapper.keyword_dict.keys())
-		print(result_by_words)
-		
-		for word in result_by_words:
-			for i in range(result_by_words[word]):
-				mapper.keyword_found(word)
-	
-	except sr.UnknownValueError:
-	    print("Google could not understand audio")
-	except sr.RequestError as e:
-	    print(" error; {0}".format(e))
+        result_by_words = count_keyword_usage(result['alternative'], mapper.keyword_dict.keys())
+        print(result_by_words)
+        
+        for word in result_by_words:
+            for i in range(result_by_words[word]):
+                mapper.keyword_found(word)
+    
+    except sr.UnknownValueError:
+        print("Google could not understand audio")
+    except sr.RequestError as e:
+        print(" error; {0}".format(e))
 
 
 def light_sensor_callback():
-	if GPIO.input(LIGHT_PIN):
-		mapper.light_turned_off()
-	else:
-		mapper.light_turned_on()
+    if GPIO.input(LIGHT_PIN):
+        mapper.light_turned_off()
+    else:
+        mapper.light_turned_on()
+    
 
 print("try starting")
 
@@ -70,7 +72,7 @@ mic = sr.Microphone()
 print("initialized")
 
 with mic as source:
-	r.adjust_for_ambient_noise(source)
+    r.adjust_for_ambient_noise(source)
 
 r.listen_in_background(mic, word_listening_callback)
 print("started listening")
@@ -87,8 +89,16 @@ def get_sound(indata, outdata, frames, time, status):
 
 #with sd.Stream(callback=get_sound):
   ## Don't stop waiting. Ever.
-while True:
-	sleep(60)
-	#sd.sleep(60000)
-	mapper.trigger_time()
 
+count = 0
+goal = 600
+output.start_pwm()
+
+while True:
+    sleep(0.1)
+    output.reset_pwm()
+    count += 1
+    
+    if count >= goal:
+        count = 0
+        mapper.trigger_time()
--- output.py
+++ output.py
@@ -2,17 +2,21 @@
 import RPi.GPIO as GPIO
 import time
 import serial
+import asyncio
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(12, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
 
-s = serial.Serial('/dev/ttyUSB0', 9600)
+s = serial.Serial('/dev/ttyUSB0', 115200)
+arm = GPIO.PWM(12, 1000)
 
+arm_level = 0
+old_arm_level = 0
 
 def set_led(level = 0): #0,1,2,3,4
-        print("Setting led...")
+        print("Setting led..."+str(level))
         s.close()
         s.open()
         time.sleep(5)
@@ -20,28 +24,46 @@ def set_led(level = 0): #0,1,2,3,4
         s.close()
 
 def set_arm(level = 0): #0,1,2,3
-        print("Setting arm...")
-        arm = GPIO.PWM(12, 1000)
-        arm.start(100)
-        time.sleep(0.2)
-        if level == 0:
-            arm.ChangeDutyCycle(0)
-        if level == 1:
-            arm.ChangeDutyCycle(35)
-        if level == 2:
-            arm.ChangeDutyCycle(70)
-        if level == 3:
-            arm.CHangeDutyCycle(100)
-            
+    print("Setting arm..."+str(level))
+    global arm_level
+    arm_level = level
+
+def start_pwm():
+    arm.start(100)
+    time.sleep(0.2)
+
+def reset_pwm():
+    global arm_level, old_arm_level
+    val = 0
+
+    if arm_level == 0:
+        val = 0
+    else:
+        if old_arm_level == 0:
+            arm.ChangeDutyCycle(100)
+            time.sleep(0.2)
+    old_arm_level = arm_level
+
+    if arm_level == 1:
+        val = 35
+    if arm_level == 2:
+        val = 70
+    if arm_level == 3:
+        val = 100
+    
+    arm.ChangeDutyCycle(val)
+    time.sleep(0.2)
+
+
 def set_puke(level = 0): #0,1
-        print("Setting puke...")
+        print("Setting puke..."+str(level))
         if level == 0:
             GPIO.output(13, GPIO.LOW)
         else:
             GPIO.output(13, GPIO.HIGH)
 def shout():
-	print("BULLSHIT")
-	pass
+    print("BULLSHIT")
+    pass
 
 def beep():
-	pass
+    pass
